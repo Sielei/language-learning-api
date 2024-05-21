c4d92020ce29ec88e853e9c5da341743261ea935
Sielei Herman <hsielei@gmail.com>
2024-05-21 20:14:28 +0300

    create jwt filter which will be added to spring security filter chain to intercept request and check if they contain jwt token and then validate the token if present.

---
+++ src/main/java/com/hs/languagelearningapi/auth/JwtFilter.java
@@ -0,0 +1,45 @@
+package com.hs.languagelearningapi.auth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Objects;
+
+class JwtFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailService userDetailService;
+
+    public JwtFilter(JwtUtil jwtUtil, CustomUserDetailService userDetailService) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailService = userDetailService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authorization = request.getHeader("Authorization");
+        if (authorization == null || !authorization.startsWith("Bearer")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        final String token = Objects.requireNonNull(authorization).split(" ")[1].trim();
+        if (!jwtUtil.validateJWTToken(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String username = jwtUtil.getUsername(token);
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                username, null, userDetailService.loadUserByUsername(username).getAuthorities());
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        request.setAttribute("userId", jwtUtil.getUserIdFromJWTToken(token));
+        filterChain.doFilter(request, response);
+    }
+}
