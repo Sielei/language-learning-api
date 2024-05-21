563dc5f83b814e5ead9fa3c387acdfbee5bfe9ef
Sielei Herman <hsielei@gmail.com>
2024-05-21 20:34:02 +0300

    add component annotation to make JwtFilter class a spring managed bean

--- src/main/java/com/hs/languagelearningapi/auth/JwtFilter.java
+++ src/main/java/com/hs/languagelearningapi/auth/JwtFilter.java
@@ -7,11 +7,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Objects;
 
+@Component
 class JwtFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
     private final CustomUserDetailService userDetailService;
