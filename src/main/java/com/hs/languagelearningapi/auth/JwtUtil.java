7a718661dbb7ae7286d8dc3d8d700ad03de5b858
Sielei Herman <hsielei@gmail.com>
2024-05-21 20:07:42 +0300

    create jwt utility class that has methods for generating and validating jwt tokens

---
+++ src/main/java/com/hs/languagelearningapi/auth/JwtUtil.java
@@ -0,0 +1,56 @@
+package com.hs.languagelearningapi.auth;
+
+import com.hs.languagelearningapi.common.ApplicationConfigData;
+import com.hs.languagelearningapi.common.DTO;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+class JwtUtil {
+    private final ApplicationConfigData configData;
+    private SecretKey secretKey;
+
+    public JwtUtil(ApplicationConfigData configData) {
+        this.configData = configData;
+        this.secretKey = Keys.hmacShaKeyFor(configData.getJwtSecret().getBytes());
+    }
+
+    public String generateJWTToken(DTO.User user){
+        return Jwts.builder()
+                .issuedAt(Date.from(ZonedDateTime.now().toInstant()))
+                .issuer("Event-io")
+                .claim("userId", user.id())
+                .claim("username", user.email())
+                .expiration(Date.from(ZonedDateTime.now().plusMinutes(configData.getJwtExpiryDurationInMinutes()).toInstant()))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public boolean validateJWTToken(String token){
+        return getUsername(token) != null && isExpired(token);
+    }
+
+    private boolean isExpired(String token) {
+        Claims claims = getClaims(token);
+        return claims.getExpiration().after(new Date(System.currentTimeMillis()));
+    }
+
+    public String getUsername(String token) {
+        Claims claims = getClaims(token);
+        return claims.get("username").toString();
+    }
+    private Claims getClaims(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload();
+    }
+    public UUID getUserIdFromJWTToken(String token){
+        return UUID.fromString(getClaims(token).get("userId").toString());
+    }
+}
