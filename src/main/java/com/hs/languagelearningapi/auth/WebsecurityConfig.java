24654bd22ffa4d5c716c71bf3833947b9ee8233d
Sielei Herman <hsielei@gmail.com>
2024-05-22 12:18:40 +0300

    add jwt filter to the filter chain and permit unauthorized requests to authentication routes, swagger ui routes, error routes and redoc route

--- src/main/java/com/hs/languagelearningapi/auth/WebsecurityConfig.java
+++ src/main/java/com/hs/languagelearningapi/auth/WebsecurityConfig.java
@@ -4,12 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 class WebsecurityConfig {
+    private final JwtFilter jwtFilter;
+
+    WebsecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
 
     @Bean
     BCryptPasswordEncoder bCryptPasswordEncoder(){
@@ -20,5 +30,19 @@ class WebsecurityConfig {
     AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
             throws Exception{
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/auth/**", "/api/v1/errors/**",
+                                "/swagger-ui/*", "/v3/api-docs/swagger-config", "/v3/api-docs",
+                                "/redoc", "/favicon.ico")
+                        .permitAll().anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }
