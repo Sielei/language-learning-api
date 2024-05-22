867741a103e821a882a36f5fb8e0a92f843bdb18
Sielei Herman <hsielei@gmail.com>
2024-05-22 16:51:45 +0300

    create lesson JPA entity

---
+++ src/main/java/com/hs/languagelearningapi/lesson/Lesson.java
@@ -0,0 +1,24 @@
+package com.hs.languagelearningapi.lesson;
+
+import com.hs.languagelearningapi.common.DTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "lessons")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+class Lesson {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+    private String name;
+    private String description;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "language_id", nullable = false)
+    private Language language;
+    @Enumerated(EnumType.STRING)
+    private DTO.LanguageProficiency languageProficiency;
+}
