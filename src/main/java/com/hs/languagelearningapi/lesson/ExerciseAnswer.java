27be3500760344bb520a52594688c80458372036
Sielei Herman <hsielei@gmail.com>
2024-05-23 04:22:55 +0300

    create ExerciseAnswer JPA entity

---
+++ src/main/java/com/hs/languagelearningapi/lesson/ExerciseAnswer.java
@@ -0,0 +1,24 @@
+package com.hs.languagelearningapi.lesson;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "exercise_answers")
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor @Builder
+public class ExerciseAnswer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exercise_id", nullable = false)
+    private Exercise exercise;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "multiple_choice_id", nullable = true)
+    private MultipleChoice multipleChoice;
+    private String answerText;
+
+}
