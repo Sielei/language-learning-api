12b354d1e9c6be405cd9eb3a14a59391df4437e5
Sielei Herman <hsielei@gmail.com>
2024-05-23 08:08:35 +0300

    create AnswerAttempt JPA entity

---
+++ src/main/java/com/hs/languagelearningapi/lesson/AnswerAttempt.java
@@ -0,0 +1,23 @@
+package com.hs.languagelearningapi.lesson;
+
+import com.hs.languagelearningapi.common.DTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "answer_attempts")
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor @Builder
+public class AnswerAttempt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+    private UUID userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exercise_id", nullable = false)
+    private Exercise exercise;
+    private String userAnswer;
+    private DTO.AttemptStatus status;
+}
