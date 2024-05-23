3c73559bfb16fe2a481067c6cef67e9d1846724f
Sielei Herman <hsielei@gmail.com>
2024-05-24 02:01:15 +0300

    create DTOs

--- src/main/java/com/hs/languagelearningapi/common/DTO.java
+++ src/main/java/com/hs/languagelearningapi/common/DTO.java
@@ -9,7 +9,10 @@ import java.util.List;
 import java.util.UUID;
 
 public class DTO {
-    public record RegisterUserRequest(String firstName, String lastName, String email, String password){}
+    public record RegisterUserRequest(@NotBlank(message = "First name is required") String firstName,
+                                      @NotBlank(message = "Last name is required") String lastName,
+                                      @Email(message = "Email should be valid") String email,
+                                      @NotBlank(message = "Password is required") String password){}
     public record RegisterUserResponse(UUID id, String name, String email){}
     public enum Role{
         TUTOR, LEARNER
@@ -30,6 +33,9 @@ public class DTO {
     public record UpdatePasswordCommand(UUID userId, String newPassword) {}
     public record CreatePasswordResetTokenCommand(UUID userId, String token) {}
     public record GetResetTokenResponse(String success, String message){}
+    public record UpdateUserRequest(@NotBlank(message = "First name is required") String firstName,
+                                    @NotBlank(message = "Last name is required") String lastName,
+                                    @Email(message = "Email should be valid") String email){}
     public record UpdatePasswordRequest(@NotBlank(message = "Current password is required") String currentPassword,
                                         @NotBlank(message = "New password is required")String newPassword) {}
     public record RefreshToken(String token, String type) {}
@@ -39,20 +45,27 @@ public class DTO {
     public record PagedCollection<T>(List<T> data, long totalElements, Integer pageNumber, Integer totalPages,
                                      @JsonProperty("isFirst") boolean isFirst, @JsonProperty("isLast") boolean isLast,
                                      @JsonProperty("hasNext") boolean hasNext, @JsonProperty("hasPrevious") boolean hasPrevious){}
-    public record LessonRequest(UUID languageId, LanguageProficiency languageProficiency, String name, String description){}
-    public record LessonResponse(UUID id, String name, String description, String language,
+    public record LessonRequest(UUID languageId, LanguageProficiency languageProficiency, Integer index,
+                                Integer percentagePassMark, String name, String description){}
+    public record LessonResponse(UUID id, Integer index, String name, String description, String language,
                                  LanguageProficiency languageProficiency){}
-    public record MultipleChoiceDto(Integer index, boolean isMatchingChoice, boolean isLeftChoice,
-                                 String choice){}
-    public record Answer(UUID multipleChoice, String answerText){}
+    public record MultipleChoiceDto(Integer index, String choice){}
+    public record MatchingChoice(Integer index, String choice){}
+    public record Answer(List<Integer> multipleChoices, List<List<String>> matchingPairs, String answerText){}
     public record ExerciseRequest(Integer index, String task, Integer taskScore, TaskType taskType,
-                                  List<MultipleChoiceDto> multipleChoices, List<Answer> answers){}
+                                  List<MultipleChoiceDto> multipleChoices, List<MatchingChoice> matchingChoices,
+                                  Answer answer){}
     public record CreateExerciseResponse(UUID id, Integer index, String task, Integer taskScore, TaskType taskType,
-                                         List<MultipleChoiceDto> multipleChoices, List<Answer> answers){}
+                                         List<MultipleChoiceDto> multipleChoices){}
     public record ExerciseResponse(UUID id, Integer index, String task, Integer taskScore, TaskType taskType,
                                          List<MultipleChoiceDto> multipleChoices){}
     public enum TaskType {
-        MULTIPLE_CHOICE_SINGLE_ANSWER, MULTIPLE_CHOICE_MULTIPLE_ANSWERS, TRUE_OR_FALSE, SHORT_ANSWER, MATCHING
+        MULTIPLE_CHOICE_SINGLE_ANSWER, MULTIPLE_CHOICE_MULTIPLE_ANSWERS, SHORT_ANSWER, MATCHING
     }
+
+    public enum AttemptStatus {
+        PASSED, FAILED, PARTIAL_FAIL
+    }
+    public record ResponseFeedback(String status, Integer score){}
 
 }
