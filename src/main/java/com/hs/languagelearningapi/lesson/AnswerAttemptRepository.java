4ec6bd570f96b48936b185d44aaf09e05ed52898
Sielei Herman <hsielei@gmail.com>
2024-05-24 01:56:56 +0300

    add JPQL query to get failed exercises and current user score

---
+++ src/main/java/com/hs/languagelearningapi/lesson/AnswerAttemptRepository.java
@@ -0,0 +1,16 @@
+package com.hs.languagelearningapi.lesson;
+
+import com.hs.languagelearningapi.common.DTO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.UUID;
+
+public interface AnswerAttemptRepository extends JpaRepository<AnswerAttempt, UUID> {
+
+    @Query("select count (a) from AnswerAttempt a where a.userId =?1 and a.exercise.lesson.id = ?2 and a.status=?3")
+    Integer findFailedExercises(UUID userId, UUID lessonId, DTO.AttemptStatus status);
+
+    @Query("select sum(a.marksAwarded) from AnswerAttempt a where a.userId = ?1 and a.exercise.lesson.id = ?2")
+    Integer findCurrentUserScore(UUID userId, UUID lessonId);
+}
